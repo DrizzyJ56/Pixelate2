13d900cccd6c8cbabd28f89578cdbefdf5bc02f6
DrizzyJ56 <blevins.j921@gmail.com>
2023-02-06 11:43:23 -0500

    made pixelate work according to project guidelines, working on additional features

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // Your code here
+const tableGrab = document.querySelector("table")
+const buttonGrab = document.getElementById('add-row')
+const selectGrab = document.querySelector("select")
+let selectedColor = 'red'
+
+function makeRow(){
+    let tr = document.createElement('tr')
+    for(let i = 0; i < 20; i++){
+        tr.appendChild(document.createElement('td')) 
+    }
+    tableGrab.appendChild(tr)
+}
+buttonGrab.addEventListener('click', makeRow)
+
+function colorize(evt){
+    let clicked = evt.target
+    if(clicked !== tableGrab){
+        if(clicked.className.length){
+            clicked.className = ''
+        }else{
+            clicked.className = selectedColor
+        }
+    }
+}
+selectGrab.addEventListener('change', function(evt){
+    selectedColor = evt.target.value
+})
+
+
+
+tableGrab.addEventListener('click', colorize)
+// tableGrab.addEventListener('click', paint)
+
